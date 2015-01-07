c77add56367a4159d03003611635062b3f731efc
sukan <sukan@b68af3b0-0fd0-467e-a851-377358ec1990>
2015-01-07 01:12:12 +0000

    1、日常提交

--- brokerAdmin/angular-scaffold-master/app/modules/activity/controllers.js
+++ brokerAdmin/angular-scaffold-master/app/modules/activity/controllers.js
@@ -17,6 +17,9 @@ activityModule.controller('actlistController', ['$scope', '$state', 'activitySer
 				$scope.list = res.list;
 				$scope.total = res.total;
 				$scope.size = res.size;
+			},function(rej) {
+				$scope.processing = false;
+				$scope.loading = false;
 			});
 		};
 	}
@@ -27,25 +30,24 @@ activityModule.controller('actlistController', ['$scope', '$state', 'activitySer
 activityModule.controller('acteditController', ['$scope', '$state','$stateParams','$modal', 'activityService',
 	function($scope, $state,$stateParams,$modal, service) {
 		var id=$stateParams.id;
-		$scope.isEdit=true;
-		$scope.started=true;
+		$scope.isEdit=true;//编辑状态
+		$scope.started=false;//活动开始状态
 		$scope.activity={};
-		if(id!=-1){
+		if(id!=-1){/*编辑活动*/
 			$scope.isEdit=false;
 			var params={"id":id};
-			// service.activityDetail(params).then(function(res) {
-			// 	// body...
-					// if(new Date()>res.startAt){
-					// 	$scope.started=true;
-					// }
-			// })
+			service.activityDetail(params).then(function(res) {
+				if(new Date()>res.startAt){
+					$scope.started=true;
+				}
+			})
 		}
 		$scope.addAward=function(argument) {
 			$modal.open({
 				templateUrl:'modules/activity/templates/activity.edit.rule.html',
 				controller:['$scope',function(scope) {
 					// body...
-					scope.award={};
+					scope.entity={};
 
 					scope.confirm=function(){
 
@@ -65,7 +67,7 @@ activityModule.controller('acteditController', ['$scope', '$state','$stateParams
 				controller:['$scope',function(scope) {
 					// body...
 
-					scope.award=$scope.activity[index];
+					scope.entity=$scope.activity[index];
 
 					scope.confirm=function(){
 
@@ -98,53 +100,177 @@ activityModule.controller('acteditController', ['$scope', '$state','$stateParams
 ]);
 
 //中奖管理
-activityModule.controller('awardlistController', ['$scope', '$state', 'activityService',
-	function($scope, $state, service) {
-
+activityModule.controller('awardlistController', ['$scope', '$state', 'activityService','selectService'
+	function($scope, $state, service,selectService) {
+		$scope.selected=false;
+		$scope.page = 1;
+		$scope.search = function(keep) {
+			var params = {};
+			if (!keep) {
+				$scope.page = 1;
+			}
+			params["page"] = $scope.page;
+			params["size"] = 10;
+			$scope.processing = true;
+			$scope.loading = true;
+			service.awardList().then(function(rec) {
+				$scope.processing = false;
+				$scope.loading = false;
+				$scope.list = selectService.init(res.list);
+				$scope.total = res.total;
+				$scope.size = res.size;
+			},function(rej) {
+				$scope.processing = false;
+				$scope.loading = false;
+			});
+		};
+		$scope.selectAll=function() {
+			selectService.selectAll($scope.selected);
+		}
 	}
 ]);
 
 //商户管理
-activityModule.controller('busilistController',  ['$scope', '$state','$stateParams','$modal', 'activityService',
-	function($scope, $state,$stateParams,$modal, service) {
+activityModule.controller('busilistController',  ['$scope', '$state','$stateParams','$modal', 'activityService','selectService'
+	function($scope, $state, $stateParams,$modal,service,selectService) {
+		$scope.selected=false;
+		$scope.page = 1;
+		$scope.search = function(keep) {
+			var params = {};
+			if (!keep) {
+				$scope.page = 1;
+			}
+			params["page"] = $scope.page;
+			params["size"] = 10;
+			$scope.processing = true;
+			$scope.loading = true;
+			service.businessList().then(function(rec) {
+				$scope.processing = false;
+				$scope.loading = false;
+				$scope.list = selectService.init(res.list);
+				$scope.total = res.total;
+				$scope.size = res.size;
+			},function(rej) {
+				$scope.processing = false;
+				$scope.loading = false;
+			});
+		};
 
-		$scope.add=function(argument) {
+		$scope.add=function() {
 			$modal.open({
 				templateUrl:'modules/activity/templates/business.edit.html',
 				controller:['$scope',function($scope) {
-					// body...
+					scope.entity={};
 				}]
 			})
+		}		
+		$scope.edit=function(item) {
+			$modal.open({
+				templateUrl:'modules/activity/templates/business.edit.html',
+				controller:['$scope',function($scope) {
+					scope.entity=item;
+				}]
+			})
+		}
+		$scope.selectAll=function() {
+			selectService.selectAll($scope.selected);
 		}
 	}
 ]);
 
 //道具管理
-activityModule.controller('itemlistController', ['$scope', '$state','$stateParams','$modal', 'activityService',
-	function($scope, $state,$stateParams,$modal, service) {
+activityModule.controller('itemlistController', ['$scope', '$state','$stateParams','$modal', 'activityService','selectService'
+	function($scope, $state, $stateParams,$modal,service,selectService) {
+		$scope.selected=false;
+		$scope.page = 1;
+		$scope.search = function(keep) {
+			var params = {};
+			if (!keep) {
+				$scope.page = 1;
+			}
+			params["page"] = $scope.page;
+			params["size"] = 10;
+			$scope.processing = true;
+			$scope.loading = true;
+			service.itemList().then(function(rec) {
+				$scope.processing = false;
+				$scope.loading = false;
+				$scope.list = selectService.init(res.list);
+				$scope.total = res.total;
+				$scope.size = res.size;
+			},function(rej) {
+				$scope.processing = false;
+				$scope.loading = false;
+			});
+		};
 
-		$scope.add=function(argument) {
+		$scope.add=function() {
 			$modal.open({
 				templateUrl:'modules/activity/templates/item.edit.html',
 				controller:['$scope',function($scope) {
-					// body...
+					scope.entity={};
 				}]
 			})
+		}
+		$scope.edit=function(item) {
+			$modal.open({
+				templateUrl:'modules/activity/templates/item.edit.html',
+				controller:['$scope',function($scope) {
+					scope.entity=item;
+				}]
+			})
+		}
+		$scope.selectAll=function() {
+			selectService.selectAll($scope.selected);
 		}
 	}
 ]);
 
 //卡券管理
-activityModule.controller('ticketlistController', ['$scope', '$state','$stateParams','$modal', 'activityService',
-	function($scope, $state,$stateParams,$modal, service) {
+activityModule.controller('ticketlistController', ['$scope', '$state','$stateParams','$modal', 'activityService','selectService'
+	function($scope, $state, $stateParams,$modal,service,selectService) {
+		$scope.selected=false;
+		$scope.page = 1;
+		$scope.search = function(keep) {
+			var params = {};
+			if (!keep) {
+				$scope.page = 1;
+			}
+			params["page"] = $scope.page;
+			params["size"] = 10;
+			$scope.processing = true;
+			$scope.loading = true;
+			service.ticketList().then(function(rec) {
+				$scope.processing = false;
+				$scope.loading = false;
+				$scope.list = selectService.init(res.list);
+				$scope.total = res.total;
+				$scope.size = res.size;
+			},function(rej) {
+				$scope.processing = false;
+				$scope.loading = false;
+			});
+		};
 
-		$scope.add=function(argument) {
+
+		$scope.add=function() {
 			$modal.open({
 				templateUrl:'modules/activity/templates/ticket.edit.html',
 				controller:['$scope',function($scope) {
-					// body...
+					scope.entity={};
 				}]
 			})
 		}
+		$scope.edit=function(item) {
+			$modal.open({
+				templateUrl:'modules/activity/templates/ticket.edit.html',
+				controller:['$scope',function($scope) {
+					scope.entity=item;
+				}]
+			})
+		}
+		$scope.selectAll=function() {
+			selectService.selectAll($scope.selected);
+		}
 	}
-]);
+]);chengz
