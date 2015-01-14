efc7e7561ea173e89ec3106ff0ac063ca22dc086
renhaili <renhaili@b68af3b0-0fd0-467e-a851-377358ec1990>
2015-01-14 11:42:56 +0000

    1.二维码列表填加，接口调试

--- brokerAdmin/angular-scaffold-master/app/modules/brokers/filters.js
+++ brokerAdmin/angular-scaffold-master/app/modules/brokers/filters.js
@@ -34,3 +34,36 @@ brokersModule.filter("brokerStatus",function(){
       	return ret;
   };
 })
+brokersModule.filter("codeStatus",function(){
+  return function (a) {
+      a=a*1;
+      var ret="";
+      switch(a){
+        case -1:
+          ret="已停用";
+          break;
+        case 0:
+          ret="全部";
+          break;
+          case 1:
+          ret="已使用";
+          break;
+          case 2:
+          ret="未使用";
+          break;
+        default:
+          ret="全部";
+          break;
+      }
+        return ret;
+  };
+})
+brokersModule.filter("userTime",function(){
+  return function (a) {
+      if (a == "-1") {
+        return "";
+      }else{
+        return "date:'yyyy-MM-dd HH:mm'";
+      };
+  };
+})
